d1c7f60e732e53370209a8597f1dfdaf1604542e
Tshivhidzo Mungomeni <lomun@amazon.com>
2026-08-13 18:07:58 +0200

    initial commit project

---
+++ src/main/java/com/substring/spingbootapp/SpingBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.substring.spingbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingBootAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/substring/spingbootapp/SpingBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.substring.spingbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
